e144b454632b057d15c44836202455086bd7f999
Win_9 <krrr883@gmail.com>
2022-08-21 22:58:08 +0900

    add initial test dummy data

---
+++ src/main/java/time/management/TestDbInit.java
@@ -0,0 +1,68 @@
+package time.management;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import time.management.domain.*;
+import time.management.service.MajorService;
+import time.management.service.MemberService;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@RequiredArgsConstructor
+public class TestDbInit {
+
+    private final TestInit testInit;
+
+    @PostConstruct
+    public void init(){
+        testInit.dummyInit1();
+        testInit.dummyInit2();
+    }
+
+
+    @Component
+    @RequiredArgsConstructor
+    @Transactional
+    static class TestInit {
+        private final MemberService memberService;
+        private final MajorService majorService;
+
+        public void dummyInit1(){
+            Major major = createMajor("TestMajor1");
+            majorService.joinMajor(major);
+
+            Member member = createMember("2018xxxx", "Name1", 1,
+                    major, Position.부원, "010-xxxx-xxxx", StudentStatus.재학,
+                    Gender.남, new CountInfo(0, 0, 0));
+            memberService.joinMember(member);
+        }
+
+        public void dummyInit2(){
+            Major major = createMajor("TestMajor2");
+            majorService.joinMajor(major);
+
+            Member member = createMember("2019xxxx", "Name2", 2,
+                    major, Position.부원, "010-xxxx-xxxx", StudentStatus.휴학,
+                    Gender.여, new CountInfo(0, 0, 0));
+            memberService.joinMember(member);
+        }
+
+        private Member createMember(String studentId, String name, int grade, Major major, Position position, String phoneNumber, StudentStatus studentStatus, Gender gender, CountInfo countInfo) {
+            Member member = new Member();
+            member.createBasicMember(studentId, name, grade, major);
+            member.addDetails(position, phoneNumber, studentStatus, gender, countInfo);
+            return member;
+        }
+
+        private Major createMajor(String majorName) {
+            Major major = new Major();
+            major.createBasicMajor(majorName);
+            return major;
+        }
+
+    }
+
+}
