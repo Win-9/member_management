5ea8ae8602faaba88cf5cfeecdf05022c6a29952
Win_9 <krrr883@gmail.com>
2022-08-30 21:09:31 +0900

    add AttendCountDto

---
+++ src/main/java/time/management/dto/MemberAttendCountDto.java
@@ -0,0 +1,10 @@
+package time.management.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberAttendCountDto {
+    private int attendCount;
+    private int quizCount;
+    private int questionCount;
+}
