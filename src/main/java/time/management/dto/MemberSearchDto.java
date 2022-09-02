2a4dd77ffeae64984e72ed2d1d7ad3ce8060e0ef
Win_9 <krrr883@gmail.com>
2022-09-03 01:01:32 +0900

    add search qualification

---
+++ src/main/java/time/management/dto/MemberSearchDto.java
@@ -0,0 +1,10 @@
+package time.management.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberSearchDto {
+    private Integer grade;
+    private String studentID;
+    private String major;
+}
