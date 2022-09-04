28d6f803cda9ab7051ad922acd037e1a596120d5
Win_9 <krrr883@gmail.com>
2022-09-05 00:06:45 +0900

    add dto field

--- src/main/java/time/management/dto/MemberSearchDto.java
+++ src/main/java/time/management/dto/MemberSearchDto.java
@@ -1,10 +1,13 @@
 package time.management.dto;
 
 import lombok.Data;
+import time.management.domain.Position;
 
 @Data
 public class MemberSearchDto {
     private Integer grade;
     private String studentID;
     private String major;
+    private String name;
+    private Position position;
 }
