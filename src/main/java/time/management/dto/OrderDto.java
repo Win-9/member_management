9e2ef0c9bbb88f3779b7994076f421f7bd88ddfc
Win_9 <krrr883@gmail.com>
2022-09-17 00:01:25 +0900

    add default variable

--- src/main/java/time/management/dto/OrderDto.java
+++ src/main/java/time/management/dto/OrderDto.java
@@ -7,6 +7,6 @@ import javax.persistence.Entity;
 
 @Data
 public class OrderDto {
-    private String sortBase;
-    private String orderOption;
+    private String sortBase = "이름";
+    private String orderOption = "오름차순";
 }
