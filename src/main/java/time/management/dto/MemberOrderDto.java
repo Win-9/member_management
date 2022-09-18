1e01296442c6a599311c817586334614f26fa431
Win_9 <krrr883@gmail.com>
2022-09-19 01:09:52 +0900

    refactoring

--- src/main/java/time/management/dto/MemberOrderDto.java
+++ src/main/java/time/management/dto/MemberOrderDto.java
@@ -3,10 +3,8 @@ package time.management.dto;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-
 @Data
-public class OrderDto {
+public class MemberOrderDto {
     private String sortBase = "이름";
     private String orderOption = "오름차순";
 }
